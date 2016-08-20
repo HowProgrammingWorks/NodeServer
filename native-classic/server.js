9d71bfc205e265342fe3f66490ca0a6e54ca0e42
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2016-08-20 15:01:00 +0300

    Added classic example from docs

---
+++ native-classic/server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
