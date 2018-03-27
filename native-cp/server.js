bcf7541a1a85709b90f74d50dc8120762fc47603
tshemsedinov <timur.shemsedinov@gmail.com>
2018-03-27 21:58:24 +0300

    Distinct ports for processes, client-side balancing

---
+++ native-cp/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const cp = require('child_process');
+const os = require('os');
+
+const pid = process.pid;
+const count = os.cpus().length;
+
+console.log(`Master pid: ${pid}`);
+console.log('Starting ' + count + ' forks');
+for (let i = 0; i < count;) cp.fork('./worker.js', [++i]);
---
+++ native-cp/worker.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const http = require('http');
+
+const BASE_PORT = 2000;
+
+const pid = process.pid;
+const id = parseInt(process.argv[2], 10);
+const port = BASE_PORT + id - 1;
+const user = { name: 'jura', age: 22 };
+
+const routing = {
+  '/': 'welcome to homepage',
+  '/user': user,
+  '/user/name': () => user.name,
+  '/user/age': () => user.age
+};
+
+const types = {
+  object: o => JSON.stringify(o),
+  string: s => s,
+  number: n => n.toString(),
+  undefined: () => 'not found',
+  function: (fn, par, client) => JSON.stringify(fn(client, par))
+};
+
+console.log(`Worker: ${id}, pid: ${pid}, port: ${port}`);
+http.createServer((req, res) => {
+  const data = routing[req.url];
+  const type = typeof(data);
+  const serializer = types[type];
+  res.setHeader('Process-Id', process.pid);
+  res.end(serializer(data, req, res));
+}).listen(port);
