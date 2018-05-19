f7868734355288a8fdf59593e09e7f92087dcd5e
tshemsedinov <timur.shemsedinov@gmail.com>
2018-05-19 10:09:48 +0300

    IP-sticky moved from repo InterProcessCommunication
    https://github.com/HowProgrammingWorks/InterProcessCommunication

---
+++ ip-sticky/server.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const os = require('os');
+const net = require('net');
+const http = require('http');
+const cluster = require('cluster');
+const cpus = os.cpus().length;
+
+if (cluster.isMaster) {
+
+  console.log(`Master pid: ${process.pid}`);
+  console.log('Starting ' + cpus + ' forks');
+
+  const workers = [];
+
+  for (let i = 0; i < cpus; i++) {
+    const worker = cluster.fork();
+    workers.push(worker);
+  }
+
+  const ipToInt = ip => (
+    ip.split('.').reduce((res, item) => (res << 8) + (+item), 0)
+  );
+
+  const balancer = (socket) => {
+    const ip = ipToInt(socket.remoteAddress);
+    const id = Math.abs(ip) % cpus;
+    const worker = workers[id];
+    if (worker) worker.send({ name: 'socket' }, socket);
+  };
+
+  const server = new net.Server(balancer);
+  server.listen(2000);
+
+} else {
+
+  console.log(`Worker pid: ${process.pid}`);
+
+  const dispatcher = (req, res) => {
+    console.log(req.url);
+    res.setHeader('Process-Id', process.pid);
+    res.end('Hello from worker ' + process.pid);
+  };
+
+  const server = http.createServer(dispatcher);
+  server.listen(null);
+
+  process.on('message', (message, socket) => {
+    if (message.name === 'socket') {
+      socket.server = server;
+      server.emit('connection', socket);
+    }
+  });
+
+}
